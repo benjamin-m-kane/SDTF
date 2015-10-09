b30bae9f7fec3f041b8e3d0d2ae1a42b55fe006d
Michael Madsen <michael.madsen@sky.com>
2015-10-09 16:42:52 +0100

    initial commit with early beginnings of deployment script

---
+++ core/setup-files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#change these values to reflect your system. Run this script and personalised versions of the service files will be saved to the directory you provide here
+saveDir=/home/mma68/sdtffiletest/
+rethinkAuthkey='YOUR_RETHINKDB_KEY_HERE_IF_ANY'
+rethinkPublicIP='10.76.71.64'
+authURL='https://stf.example.org/auth/mock/'
+websocketURL='https://stf.example.org/'
+appURL='https://stf.example.org/'
+appTriproxyURL='appside.stf.example.org'
+devTriproxyURL='devside.stf.example.org'
+storageURL='https://stf.example.org/'
+sessionSecret='YOUR_SESSION_SECRET_HERE'
+
+
+#Copy the original files to the given directory
+
+
+#Replace the variables with the given values in the new files
+
+#Enable them with systemctl
+
+#Start them
