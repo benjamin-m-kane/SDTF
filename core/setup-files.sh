3fd7e07ea0e30d076bffba02226c1250ffb59f24
Michael Madsen <michael.madsen@sky.com>
2015-10-13 16:21:02 +0100

    Added functionality to setup-files.sh

--- core/setup-files.sh
+++ core/setup-files.sh
@@ -13,7 +13,31 @@ storageURL='https://stf.example.org/'
 sessionSecret='YOUR_SESSION_SECRET_HERE'
 
 
+#Define the function to replace text in-place in a file
+replace() {
+    local search=$1
+    local replace=$2
+    local file=$3
+    sed -i "s/${search}/${replace}/g" ${file}
+}
+
 #Copy the original files to the given directory
+#Runs through all items in current directory
+#If item ends in .service and is a file, copy it
+for i in * 
+do
+    if [[ $i == *.service ]]
+    then
+        if test -f "$i" 
+        then
+           echo "Copying $i to $saveDir"
+           cp $i $saveDir
+        fi
+    fi
+done
+
+
+
 
 
 #Replace the variables with the given values in the new files
